99ac3708f75630ca7e3e9a959affc29a29fe3c61
khisa318 <khisaarmstrong@gmail.com>
2026-06-27 11:38:59 +0300

    refactor: wrap authentication routes with PublicOnlyRoute layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,68 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext"; 
+
+// YOUR EXISTING STOREFRONT PAGES
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 import CategoryPage from "./pages/CategoryPage";
+import CheckOut from "./pages/CheckOut";
+import AuthPage from "./pages/AuthPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import OrderTracking from "./pages/OrderTracking";
+import FAQ from "./pages/FAQ";
+import Policies from "./pages/Policies";
+import SearchResults from "./pages/SearchResults";
+
+// SECURITY DASHBOARD CORRIDORS
 import AdminDashboard from "./pages/AdminDashboard";
 import CustomerProfile from "./pages/CustomerProfile";
-import { AdminProtectedRoute, CustomerProtectedRoute } from "./components/ProtectedRoutes";
+import NotFoundPage from "./pages/NotFoundPage";
+
+// IMPORT PROTECTION FILTERS
+import { AdminProtectedRoute, CustomerProtectedRoute, PublicOnlyRoute } from "./components/ProtectedRoutes";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes anyone can see */}
-        <Route path="/shop" element={<CategoryPage />} />
+    <CartProvider>
+      <Router>
+        <div className="selection:bg-[#ff6b2b]/20">
+          <Routes>
+            
+            {/* PUBLIC OPEN STOREFRONT VIEWS */}
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/shop" element={<CategoryPage />} />
+            <Route path="/checkout" element={<CheckOut />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/track-order" element={<OrderTracking />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/policies" element={<Policies />} />
+            <Route path="/search" element={<SearchResults />} />
 
-        {/* Protected Customer Routes (Profile, Order History) */}
-        <Route element={<CustomerProtectedRoute />}>
-          <Route path="/account/profile" element={<CustomerProfile />} />
-        </Route>
+            {/* GUEST ONLY ACCESS GATEWAY (Intercepts and runs role navigation checks) */}
+            <Route element={<PublicOnlyRoute />}>
+              <Route path="/account" element={<AuthPage />} />
+            </Route>
 
-        {/* Protected Admin Routes (Products Management, Global Tracking) */}
-        <Route element={<AdminProtectedRoute />}>
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* SECURE CUSTOMER DOMAIN */}
+            <Route element={<CustomerProtectedRoute />}>
+              <Route path="/account/profile" element={<CustomerProfile />} />
+            </Route>
+
+            {/* SECURE ADMINISTRATIVE CONTROL PANEL */}
+            <Route element={<AdminProtectedRoute />}>
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            </Route>
+
+            {/* ERROR CATCH-ALL */}
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
